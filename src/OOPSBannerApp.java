7c3d2e0a5ad02a062661b035514ab61acb9d1556
nivicodes0712 <nivithasree0712@gmail.com>
2026-03-02 09:31:34 +0530

    added

---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp
+ * 
+ * UC1: Prints the literal text "OOPS" to the console.
+ * This is the foundational step for the OOPS Banner App project.
+ * 
+ * @author NIVITHA
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
