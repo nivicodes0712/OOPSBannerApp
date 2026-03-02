40d07698d0adf908f9b600d5ce7c2052b482f97d
nivicodes0712 <nivithasree0712@gmail.com>
2026-03-02 11:11:27 +0530

    Implemented UC3

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,28 +1,16 @@
-/**
- * OOPSBannerApp
- * 
- * UC2: Displays "OOPS" as a 7-line ASCII banner
- * using spaces and '*' characters.
- * 
- * @author NIVITHA
- * @version 2.0
- */
-
 public class OOPSBannerApp {
-
-    /**
-     * Main method - Entry point of the application
-     * @param args Command line arguments
-     */
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****    *****    ***** ");
-        System.out.println("*     *  *     *  *     *  *      ");
-        System.out.println("*     *  *     *  *     *  *      ");
-        System.out.println("*     *  *     *  *     *  *****  ");
-        System.out.println("*     *  *     *  *     *      *  ");
-        System.out.println("*     *  *     *  *     *      *  ");
-        System.out.println(" *****    *****    *****    ***** ");
+        String banner = String.join("\n",
+            "  ***      ***    ******    ***** ",
+            "**   **  **   **  **   **  **   ",
+            "**   **  **   **  **   **  **   ",
+            "**   **  **   **  ******    ***** ",
+            "**   **  **   **  **            **",
+            "**   **  **   **  **             **",
+            "  ***      ***    **        ***** "
+        );
 
+        System.out.println(banner);
     }
 }
